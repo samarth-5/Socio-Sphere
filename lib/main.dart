68d7a58ca5fac35c2e63cc7e24160ae151f2b681
Samarth Narayan <kayaniv321@gmail.com>
2024-06-22 01:56:13 +0530

    firebase setup completed and dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:socio_sphere/responsive/mobile_screen_layout.dart';
 import 'package:socio_sphere/responsive/responsive_layout_screen.dart';
 import 'package:socio_sphere/responsive/web_Screen_layout.dart';
 import 'package:socio_sphere/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyDYiW5ewySrgjZwwvWgf7Jgb1lzoK877_w",
+            appId: "1:841268398240:web:244874999c11f271dfa1c5",
+            messagingSenderId: "841268398240",
+            projectId: "socio-sphere-97215",
+            storageBucket: "socio-sphere-97215.appspot.com",));
+  } 
+  else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
